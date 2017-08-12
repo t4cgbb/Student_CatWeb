51c5803e2f2bda3dea58c85ba4d75eaeb841142c
t4cgbb <t4cgbb@gmail.com>
2017-08-13 02:19:01 +0800

    商品頁面

---
+++ cat/inclnde/header.php
@@ -0,0 +1,19 @@
+﻿        <header>
+            <div class="outside">
+                <div class="menu">
+					<div class="short_outside">
+						<a href="index.html"><img class="logo" src="img/title_left.png"></img></a>
+						<img class="w350 m_botton40" src="img/title.png"></img>
+					</div>
+                    <nav>
+                        <ul>
+                            <li class="active"><a href="../Index.php" title="Home"><span><i class="fa fa-home m_right5"></i>首頁</span></a></li>
+                            <li><a href="./" title="Pets products"><span><i class="fa fa-shopping-basket m_right5"></i>貓咪用品</span></a></li>
+                            <li><a href="./" title="Pets Salon"><span><i class="fa fa-hand-scissors-o m_right5"></i>寵物美容</span></a></li>                            
+                            <li><a href="./Cat_view.html" title="Pets information"><span><i class="fa fa-paw m_right5"></i>貓咪畫廊</span></a></li>
+                            <li><a href="./" title="Pets information"><span><i class="fa fa-commenting"></i>連絡我們</span></a></li>
+                        </ul>          
+                    </nav>
+                </div>
+            </div>
+        </header>

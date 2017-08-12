0d011519cfe09fe85a33950f8844f774d23af121
t4cgbb <t4cgbb@gmail.com>
2017-08-13 02:21:56 +0800

    貓咪家庭寵物美容生活用品館

---
+++ cat/inclnde/link.php
@@ -0,0 +1,16 @@
+﻿<!doctype html>
+<html lang="zh-TW">
+
+    <head>
+        <meta charset="utf8" />
+        <title>貓咪家庭美容寵物網</title>
+        <meta name="description" content="。養貓需求、寵物用品、寵物飼料，還是想要寵物美容？通通找貓咪家庭就對啦！">
+        <meta name="keywords" content="美容,貓狗飼料,貓咪用具">
+		<meta name="copyright" content="貓咪家庭美容寵物網著">
+		<link rel="stylesheet" href="css/main.css">
+		<link rel="icon" href="favicon.png" type="image/ico" />
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">	
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+        <script src="js/custom.js"></script>
+		<script src="js/catbar.js"></script>
+	
---
+++ cat/js/custom.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $(".remove").click(function(a) {
+        a.preventDefault();
+        $('.remind').slideUp(200);
+    });          
+});

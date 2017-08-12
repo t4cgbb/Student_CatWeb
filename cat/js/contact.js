9606872c622f88a02528a90c1884f3954dfa9e1b
t4cgbb <t4cgbb@gmail.com>
2017-08-13 02:20:17 +0800

    刪除

--- cat/js/contact.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  //Set the carousel options
-  $('#quote-carousel').carousel({
-    pause: true,
-    interval: 4000,
-  });
-});
--- cat/js/custom.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $(".remove").click(function(a) {
-        a.preventDefault();
-        $('.remind').slideUp(200);
-    });          
-});
